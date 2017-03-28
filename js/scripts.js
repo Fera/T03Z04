1a143179ddefcdfc0b8148dfd2b88e70b50278fd
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-03-28 22:08:32 +0200

    volumebar

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
 		var video = $("video").get(0),
 			playpause = $("#playpause"),
 			mute = $("#mute"),
+			mutebar = $("#mutebar"),
 			time = $("#time"),
 			progressbar = $("#progressbar"),
 			valuestep = 0;
@@ -22,6 +23,20 @@
 
 		});
 
+		mutebar.on("input change", function(){
+
+			if(mutebar.val() == 0){
+				video.volume = 0;
+			}
+
+			if(mutebar.val() == 100) {
+				video.volume = 1;
+			}
+			
+
+		});
+
+
 		mute.on("click", function(){
 	        if($("video").prop("muted")){
 	            $("video").prop("muted", false);
@@ -31,6 +46,39 @@
 	            $("video").prop("muted", true);
 	            mute.addClass("mute-off").removeClass("mute");
 	        }
+	    });
+
+	    function formatTime(seconds) {
+	        var seconds = Math.round(seconds),
+	            minutes = Math.floor(seconds / 60),
+	            remainingSeconds = seconds - minutes * 60;
+
+	        if(remainingSeconds == 0)
+	            remainingSeconds = "00";
+	        else if(remainingSeconds < 10)
+	            remainingSeconds = "0" + remainingSeconds;
+
+	        return minutes + ":" + remainingSeconds;
+	    }
+
+	    video.oncanplaythrough = function () {
+	        var curtime = formatTime(video.currentTime);
+	        var duration = formatTime(video.duration);
+	        time.html(curtime + " / " + duration);
+	    };
+
+	    video.ontimeupdate = function () {
+	        var curtime = formatTime(video.currentTime);
+	        var duration = formatTime(video.duration);
+	        time.html(curtime + " / " + duration);
+
+	        var currentProgress = (Math.round(video.currentTime) / Math.round(video.duration)) * 100;
+	        progressbar.val(currentProgress);
+	    };
+
+	    progressbar.on("input change", function() {
+	        var clickedPos = progressbar.val();
+	        video.currentTime = clickedPos;
 	    });
 
     });
